28a762ac24024d67c8fcfdd4601a0405958d3626
letsgocn <139432509+letsgocn@users.noreply.github.com>
2025-05-23 15:23:59 +0800

    add

--- src/components/contributor/Contributors.js
+++ src/components/contributor/Contributors.js
@@ -5,14 +5,6 @@ import Contributor from "./Contributor";
 
 export default function Contributors() {
     return (
-        <footer id="contributors">
-            <h3>Thank you to all who have contributed to this project!</h3>
-            <ul>
-                {
-                    contributors.map((contributor) => (<Contributor data={contributor} key={contributor.name}></Contributor>))
-                }
-            </ul>
-            <div>Sky Clock originally created by <a href="https://github.com/cmstead">Chris Stead</a>. Project source is <a href="https://github.com/cmstead/sky-clock">available on GitHub</a>.</div>
-        </footer>
+    
     )
 }
